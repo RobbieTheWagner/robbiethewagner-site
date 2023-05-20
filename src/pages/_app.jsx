961f09012db2525e9cae51fd31afa81567c2faec
Robert Wagner <rwwagner90@gmail.com>
2023-05-20 15:44:58 -0400

    Add analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef } from 'react'
 
+import { Analytics } from '@vercel/analytics/react';
+
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 
@@ -30,6 +32,7 @@ export default function App({ Component, pageProps, router }) {
         <Header />
         <main>
           <Component previousPathname={previousPathname} {...pageProps} />
+          <Analytics />
         </main>
         <Footer />
       </div>
--- src/pages/speaking.jsx
+++ src/pages/speaking.jsx
@@ -43,39 +43,32 @@ export default function Speaking() {
           <SpeakingSection title="Conferences">
             <Appearance
               href="#"
-              title="In space, no one can watch you stream — until now"
-              description="A technical deep-dive into HelioStream, the real-time streaming library I wrote for transmitting live video back to Earth."
-              event="SysConf 2021"
+              title="Title"
+              description="Description"
+              event="EmberConf 2019"
               cta="Watch video"
             />
             <Appearance
               href="#"
-              title="Lessons learned from our first product recall"
-              description="They say that if you’re not embarassed by your first version, you’re doing it wrong. Well when you’re selling DIY space shuttle kits it turns out it’s a bit more complicated."
-              event="Business of Startups 2020"
+              title="Title"
+              description="Description"
+              event="EmberConf 2018"
               cta="Watch video"
             />
           </SpeakingSection>
           <SpeakingSection title="Podcasts">
             <Appearance
-              href="#"
-              title="Using design as a competitive advantage"
-              description="How we used world-class visual design to attract a great team, win over customers, and get more press for Planetaria."
-              event="Encoding Design, July 2022"
+              href="https://whiskeywebandwhatnot.fm/"
+              title="A fireside chat with your favorite web developers"
+              description="TODO"
+              event="Whiskey Web and Whatnot"
               cta="Listen to podcast"
             />
             <Appearance
-              href="#"
-              title="Bootstrapping an aerospace company to $17M ARR"
-              description="The story of how we built one of the most promising space startups in the world without taking any capital from investors."
-              event="The Escape Velocity Show, March 2022"
-              cta="Listen to podcast"
-            />
-            <Appearance
-              href="#"
-              title="Programming your company operating system"
-              description="On the importance of creating systems and processes for running your business so that everyone on the team knows how to make the right decision no matter the situation."
-              event="How They Work Radio, September 2021"
+              href="https://shoptalkshow.com/512/"
+              title="Web Whiskey Crossover with Chuck Carpenter & Robbie Wagner"
+              description="TODO"
+              event="ShopTalk Show, April 2022"
               cta="Listen to podcast"
             />
           </SpeakingSection>
