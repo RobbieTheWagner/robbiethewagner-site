b6da0b4bdd9d6deb17929fef75df57fe2a99c214
Robert Wagner <rwwagner90@gmail.com>
2023-07-14 21:14:35 -0400

    Update speaking.jsx

--- src/pages/speaking.jsx
+++ src/pages/speaking.jsx
@@ -1,15 +1,15 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
-import { Card } from '@/components/Card'
-import { Section } from '@/components/Section'
-import { SimpleLayout } from '@/components/SimpleLayout'
+import { Card } from '@/components/Card';
+import { Section } from '@/components/Section';
+import { SimpleLayout } from '@/components/SimpleLayout';
 
 function SpeakingSection({ children, ...props }) {
   return (
     <Section {...props}>
       <div className="space-y-16">{children}</div>
     </Section>
-  )
+  );
 }
 
 function Appearance({ title, description, event, cta, href }) {
@@ -22,7 +22,7 @@ function Appearance({ title, description, event, cta, href }) {
       <Card.Description>{description}</Card.Description>
       <Card.Cta>{cta}</Card.Cta>
     </Card>
-  )
+  );
 }
 
 export default function Speaking() {
@@ -42,16 +42,35 @@ export default function Speaking() {
         <div className="space-y-20">
           <SpeakingSection title="Conferences">
             <Appearance
+              href="/"
+              title="Whiskey Web and Whatnot Live at EmberConf"
+              description="TODO"
+              event="EmberConf 2023"
+              cta="Watch video"
+            />
+            <Appearance
               href="https://www.youtube.com/watch?v=Kw0V9HvMpI4"
               title="Talking to Your Dog with Ember"
-              description="Description"
+              description={`
+              Have you ever wished you knew what your dog was saying when they were barking? 
+              Now you can! This talk showcases how we can use Ember and the Web Audio API to 
+              decipher different dog barks and translate them into something more meaningful.
+              `}
               event="EmberConf 2020"
               cta="Watch video"
             />
             <Appearance
               href="https://www.youtube.com/watch?v=u5onNDpt8ks"
               title="Ember Data - It's Not Just for JSON:API Anymore"
-              description="Description"
+              description={`
+              Have you ever been completely stumped by a nonstandard API and integrating it with Ember Data? 
+              Ember Data “just works” with JSON API out of the box, so often you do not need to know much about the magic behind the scenes. 
+              When you have a nonstandard API, however, things can get hairy quick.
+
+              This talk covers how to leverage the Ember Data hooks to bridge the gap, doing things like manually
+              adding in links and relationships, for side loading data, combining multiple API calls together into one response, 
+              changing the entire structure of your data, adding and removing default records and much, much more!
+              `}
               event="EmberConf 2019"
               cta="Watch video"
             />
@@ -60,14 +79,26 @@ export default function Speaking() {
             <Appearance
               href="https://whiskeywebandwhatnot.fm/"
               title="A fireside chat with your favorite web developers"
-              description="TODO"
+              description={`
+              Whiskey Web and Whatnot is an informal, whiskey fueled fireside chat with your favorite 
+              web devs. We discuss all things web development including JavaScript, TypeScript, EmberJS, 
+              React, Astro, SolidJS, CSS, HTML, Web3, and more, but we also get to know the human side 
+              of developers and their hobbies outside of work.
+              `}
               event="Whiskey Web and Whatnot"
+              cta="Listen to podcast"
+            />
+            <Appearance
+              href="https://changelog.com/jsparty/285"
+              title="Frontend Feud: CSS Podcast vs Whiskey Web and Whatnot"
+              description="We had a blast with this friendly competition. I still cannot believe how many people hate all C languages!"
+              event="JS Party, July 2023"
               cta="Listen to podcast"
             />
             <Appearance
               href="https://shoptalkshow.com/512/"
               title="Web Whiskey Crossover with Chuck Carpenter & Robbie Wagner"
-              description="TODO"
+              description="We had a great chat with Chris and Dave about podcasting, hiring and firing, imposter syndrome, web3, Ember, and working with clients."
               event="ShopTalk Show, April 2022"
               cta="Listen to podcast"
             />
@@ -75,5 +106,5 @@ export default function Speaking() {
         </div>
       </SimpleLayout>
     </>
-  )
+  );
 }
