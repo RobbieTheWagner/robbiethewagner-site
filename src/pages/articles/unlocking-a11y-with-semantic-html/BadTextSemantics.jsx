65df6edd1d914a929d21ba797f1da101ced57e67
Robert Wagner <rwwagner90@gmail.com>
2024-03-28 08:41:31 -0400

    Add text content example

---
+++ src/pages/articles/unlocking-a11y-with-semantic-html/BadTextSemantics.jsx
@@ -0,0 +1,19 @@
+export function BadTextSemantics() {
+  return (
+    <>
+      <span className="text-3xl">Heading</span>
+      <br />
+      This is the first section of my document.
+      <br />
+      I'll add another paragraph here too.
+      <br />
+      1. First item
+      <br />
+      2. Second item
+      <br />
+      <span className="text-2xl">Subheading</span>
+      <br />
+      This is the first subsection of my document.
+    </>
+  );
+}
